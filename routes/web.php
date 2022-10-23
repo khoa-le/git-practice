f1919a09a4b9bcd7bd3de63ccbece3d7adc4f4e8
Khoa Le <le.ngoc.khoa@oro.com>
2022-10-23 13:59:19 +0700

    add production2

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/production1', function () {
     return view('welcome');
 });
+Route::get('/production2', function () {
+    return view('welcome');
+});
+
